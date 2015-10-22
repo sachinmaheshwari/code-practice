4376bc1148f49117698bf1c82ef58b7942daefed
Sachin Maheshwari <sachin.maheshwari@hp.com>
2015-10-22 17:53:04 +0530

    Code for various puzzles

--- code/src/javaconcepts/TestInterface.java
+++ code/src/javaconcepts/TestInterface.java
@@ -6,8 +6,6 @@ package javaconcepts;
 public interface TestInterface {
 
     // static void testSomething();  not supported in 1.7 but it is supported in 1.8 and we have to provide the implementation of method as well
-
-
     class Something{
         private int abc;
 
