2f5d9235262fca18e45e3ff7c47d47ff651e39f7
Sachin Maheshwari <sachin.maheshwari8@gmail.com>
2019-01-04 12:25:22 +0530

    Adding placeholder class for big number counter problem.

---
+++ code/src/puzzles/ArrayBiggerNumberCounter.java
@@ -0,0 +1,14 @@
+package puzzles;
+
+public class ArrayBiggerNumberCounter {
+
+  public static void main(String[] args) {
+    new ArrayBiggerNumberCounter().solve(new int[] {2,4,7,9,1,3});
+  }
+
+  private void solve(int[] ints) {
+
+  }
+
+
+}
